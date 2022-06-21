459a9524809e6335d7ba27ee5eacf2e3c33f336f
JAMDev <jamdeveloper0@gmail.com>
2022-06-21 17:23:50 -0300

    Creation of the header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,25 @@
+import { MagnifyingGlass } from "phosphor-react";
+
+export function Header() {
+    return(
+        <header
+         className="
+            bg-brand-blue-850 w-full h-[120px] pb-[20px] flex justify-center items-center text-2xl
+         "
+        >
+
+            <h1
+             className="
+                flex items-center gap-x-4
+             "
+            >
+                Rick & Morty Finder
+                <MagnifyingGlass 
+                 weight="duotone"
+                 className="fill-brand-green-400"
+                 mirrored={true}
+                />
+            </h1>
+        </header>
+    )
+}
