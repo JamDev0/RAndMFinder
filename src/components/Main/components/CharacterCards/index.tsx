3d7463a0c569b5786bd00e08310ac329d5e5f7bb
JAMDev <jamdeveloper0@gmail.com>
2022-06-28 14:11:34 -0300

    Implementação do hook de tradução no CharacterCards

--- src/components/Main/components/CharacterCards/index.tsx
+++ src/components/Main/components/CharacterCards/index.tsx
@@ -1,41 +1,73 @@
-import { useGetCharactersByPageQuery } from "../../../../graphql/generated";
+//@ts-ignore
+import translate from "translate";
+translate.engine = "google";
+
+import { useEffect, useState } from "react";
+
+import { useGetCharactersByPageLazyQuery, useGetCharactersByPageQuery } from "../../../../graphql/generated";
 
 import { CharacterCard } from "./CharacterCard";
+import { useTranslateCharacters } from "../../../../hooks/useTranslateCharacters";
+
+
+interface characterInterface {
+    name: string;
+    species: string;
+    status: string;
+    gender: string;
+    image: string;
+}
 
 export function CharacterCards() {
+    const [getCharactersByPageLazyQuery, { called, loading, data }] = useGetCharactersByPageLazyQuery();
 
-    const {data, loading, error} = useGetCharactersByPageQuery({
-        variables: {
-            page: Math.floor(Math.random()*43)
+    const {translatedCharacters, isTranslating, translateCharacters} = useTranslateCharacters();
+    
+    useEffect(()=>{
+        getCharactersByPageLazyQuery({
+            variables: {
+                page: Math.floor(Math.random() * 43)
+            }
+        })
+    }, [])
+
+
+    useEffect(()=>{
+        translateCharacters(data?.characters?.results as characterInterface[]);
+    }, [data])
+
+
+
+    if(!loading && called) {
+        if(!isTranslating) {
+            return(
+                <section
+                className="
+                    flex flex-wrap gap-y-[32px] w-full
+                "
+                >
+                    {
+                        translatedCharacters.map( caracter => {
+                            return(
+                                <CharacterCard
+                                key={caracter?.image} 
+                                gender={caracter?.gender!}
+                                image={caracter?.image!}
+                                name={caracter?.name!}
+                                species={caracter?.species!}
+                                status={caracter?.status!}
+                                />
+                            )
+                        })
+                    }
+                </section>
+            )
+        } else {
+            return <p>Translating...</p>
         }
-    })
-
-    if(!loading && !error) {
-        return(
-            <section
-             className="
-                flex flex-wrap gap-y-[32px] w-full
-             "
-            >
-                {
-                    data?.characters?.results?.map( caracter => {
-                        return(
-                            <CharacterCard
-                             key={caracter?.name!} 
-                             gender={caracter?.gender!}
-                             image={caracter?.image!}
-                             name={caracter?.name!}
-                             species={caracter?.species!}
-                             status={caracter?.status!}
-                            />
-                        )
-                    })
-                }
-            </section>
-        )
+                
     }
     else {
-        return <p>Wait...</p>
+        return <p>Loading...</p>
     }
-    
 }
