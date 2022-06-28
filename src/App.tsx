f318f3293ca1c9919b73fc7f52ccef19fb1a4999
JAMDev <jamdeveloper0@gmail.com>
2022-06-28 14:07:20 -0300

    Inserção do provider do hook translateCaracters

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,17 @@ import { client } from "./client";
 import { Header } from "./components/Header";
 
 import { Main } from "./components/Main";
+import { TranslateCharactersProvider } from "./hooks/useTranslateCharacters";
 
 export function App() {
 
   return (
     <ApolloProvider client={client}>
-      <Header/>
-      
-      <Main/>
+      <TranslateCharactersProvider>
+        <Header/>
+        
+        <Main/>
+      </TranslateCharactersProvider>
     </ApolloProvider>
   )
 }
