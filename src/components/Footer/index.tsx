410936fd99f06f62eb26d82bb1ae40ee960b018a
JAMDev <jamdeveloper0@gmail.com>
2022-06-29 16:07:18 -0300

    Criação do componente Footer
    Responsável pela navegação do site

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,53 @@
+import { CaretLeft, CaretRight } from "phosphor-react"
+import { useCurrentCharactersPage } from "../../hooks/useCurrentCharactersPage"
+
+export function Footer() {
+    const { currentCharactersPage, setCurrentCharactersPage, lastCharactersPage } = useCurrentCharactersPage()
+
+    return(
+        <footer
+         className="
+            mt-[40px] mb-[50px] flex justify-center items-center
+         "
+        >
+            <div
+             className="
+                flex justify-center items-center text-3xl
+             "
+            >
+                {
+                    currentCharactersPage > 1 ?
+                        
+                        <CaretLeft
+                         weight="bold"
+                         onClick={() => {
+                            setCurrentCharactersPage(currentCharactersPage - 1)
+                         }}
+                        />
+                    :
+                            null
+                }
+                <span
+                 className="
+                  font-bold
+                 "
+                >
+                    {currentCharactersPage}
+                </span>
+
+                {
+                    lastCharactersPage !== currentCharactersPage ?
+                        <CaretRight
+                         weight="bold"
+                         onClick={() => {
+                            setCurrentCharactersPage(currentCharactersPage + 1)
+                         }}
+                        />
+                    :
+                         null
+                }
+                
+            </div>
+        </footer>
+    )
+}
