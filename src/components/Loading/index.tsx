960f846fe241cf2a90ad6a0268e00d5bca25b184
JAMDev <jamdeveloper0@gmail.com>
2022-06-29 15:00:01 -0300

    Criação do componente de loading

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,22 @@
+import { CircleNotch } from "phosphor-react";
+
+import { motion } from "framer-motion";
+
+export function Loading() {
+    return (
+        <motion.div 
+         className="
+            w-full h-auto
+         "
+         animate={{ rotate: 360 }}
+         transition={{ duration: 0.9, ease: 'backInOut', repeat: Infinity }}
+        >
+
+            <CircleNotch
+             className='
+                w-full h-auto
+             '
+            />
+        </motion.div>
+    )
+}
