7937afcc3bca1a2a5f250977c0f9d7f1dd0c2573
JAMDev <jamdeveloper0@gmail.com>
2022-06-29 15:03:16 -0300

    Implementação do provider do isLoading

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,19 @@ import { Header } from "./components/Header";
 
 import { Main } from "./components/Main";
 import { TranslateCharactersProvider } from "./hooks/useTranslateCharacters";
+import { IsLoadingProvider } from "./hooks/useIsLoading";
+import { Loading } from "./components/Loading";
 
 export function App() {
 
   return (
     <ApolloProvider client={client}>
       <TranslateCharactersProvider>
-        <Header/>
-        
-        <Main/>
+        <IsLoadingProvider>
+          <Header/>
+          
+          <Main/>
+        </IsLoadingProvider>
       </TranslateCharactersProvider>
     </ApolloProvider>
   )
