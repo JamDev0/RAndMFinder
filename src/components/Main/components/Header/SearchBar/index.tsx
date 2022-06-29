7ce383b8c2be53142b00ccd67961ef7d1acfc5b6
JAMDev <jamdeveloper0@gmail.com>
2022-06-29 15:07:34 -0300

    Altrerações na seachBar (changeLoadingState, searchBar empty)
    Definição do isLoading feita pela search bar, já que ela é responsável pela ativação das querys.
    
    Agora se a searchBar estiver vazia, será pego personagens aleatorios

--- src/components/Main/components/Header/SearchBar/index.tsx
+++ src/components/Main/components/Header/SearchBar/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 
-import { useGetCharactersByStringLazyQuery } from "../../../../../graphql/generated";
+import { useGetCharactersByPageLazyQuery, useGetCharactersByStringLazyQuery } from "../../../../../graphql/generated";
+import { useIsLoading } from "../../../../../hooks/useIsLoading";
 
 import { useTranslateCharacters } from "../../../../../hooks/useTranslateCharacters";
 
@@ -15,64 +16,85 @@ interface characterInterface {
 export function SearchBar() {
     const [searchValue, setSearchValue] = useState<string>('');
 
+    const [randomCharacters, setRandomCharacters] = useState<characterInterface[]>([] as characterInterface[]);
+
     const [characters, setCharacters] = useState<characterInterface[]>([] as characterInterface[]);
 
     const [orderedCharacters,setOrderedCharacters] = useState<characterInterface[]>([] as characterInterface[]);
 
     const [getCharactersByStringLazyQuery, { called, loading, data }] = useGetCharactersByStringLazyQuery();
 
+    const [getCharactersByPageLazyQuery, props] = useGetCharactersByPageLazyQuery()
+
     const {isTranslating, translateCharacters} = useTranslateCharacters();
 
+    const { isLoading, changeLoadingState } = useIsLoading();
+
     useEffect(()=>{
-        if(searchValue.length > 0) {
+        if(searchValue !== '') {
+            changeLoadingState(true);
             getCharactersByStringLazyQuery({variables: {
                 name: searchValue,
                 page: 1
-            }}).then( data => setCharacters(data.data?.characters?.results as characterInterface[]))
+            }}).then( da => {setCharacters(da.data?.characters?.results as characterInterface[]); setRandomCharacters([] as characterInterface[])})
+        } else {
+            changeLoadingState(true);
+            getCharactersByPageLazyQuery({variables: {
+                page: Math.floor(Math.random() * 43)
+            }}).then( da => {setRandomCharacters(da.data?.characters?.results as characterInterface[]); setCharacters([] as characterInterface[])})
         }
     }, [searchValue])
 
-
     useEffect(()=> {
-        if(data?.characters?.info?.next) {
-            getCharactersByStringLazyQuery({variables: {
-                name: searchValue,
-                page: data?.characters?.info?.next
-            }}).then( data => setCharacters( last => [...last, ...data.data?.characters?.results as characterInterface[]]))
-        } else {
-            if(characters.length > 0) {
-                let innerCharacters = Array.from(characters);
-
-                let lowercasedSearchValue = searchValue.toLocaleLowerCase()
-                
-                innerCharacters = innerCharacters.sort( (characterA, characterB) => {
-                    if(characterA.name.toLocaleLowerCase().indexOf(lowercasedSearchValue) > characterB.name.toLocaleLowerCase().indexOf(lowercasedSearchValue)) {
-                        return 1
-                    }
-        
-                    if(characterA.name.toLocaleLowerCase().indexOf(lowercasedSearchValue) < characterB.name.toLocaleLowerCase().indexOf(lowercasedSearchValue)) {
-                        return -1
-                    }
-
-                    if(characterA.name.toLocaleLowerCase() > characterB.name.toLocaleLowerCase()) {
-                        return 1;
-                    }
-
-                    if(characterA.name.toLocaleLowerCase() < characterB.name.toLocaleLowerCase()) {
-                        return -1;
-                    }
-        
-                    return 0
-                }).slice(0, 19);
-
-                setOrderedCharacters(innerCharacters);
-            }
-        } 
+        if(characters.length > 0) {
+            if(data?.characters?.info?.next) {
+                getCharactersByStringLazyQuery({variables: {
+                    name: searchValue,
+                    page: data?.characters?.info?.next
+                }}).then( data => {setCharacters( last => [...last, ...data.data?.characters?.results as characterInterface[]])})
+            } else {
+                if(characters.length > 0) {
+                    let innerCharacters = Array.from(characters);
+    
+                    let lowercasedSearchValue = searchValue.toLocaleLowerCase()
+                    
+                    innerCharacters = innerCharacters.sort( (characterA, characterB) => {
+                        if(characterA.name.toLocaleLowerCase().indexOf(lowercasedSearchValue) > characterB.name.toLocaleLowerCase().indexOf(lowercasedSearchValue)) {
+                            return 1
+                        }
+            
+                        if(characterA.name.toLocaleLowerCase().indexOf(lowercasedSearchValue) < characterB.name.toLocaleLowerCase().indexOf(lowercasedSearchValue)) {
+                            return -1
+                        }
+    
+                        if(characterA.name.toLocaleLowerCase() > characterB.name.toLocaleLowerCase()) {
+                            return 1;
+                        }
+    
+                        if(characterA.name.toLocaleLowerCase() < characterB.name.toLocaleLowerCase()) {
+                            return -1;
+                        }
+            
+                        return 0
+                    }).slice(-1, 20);
+    
+                    setOrderedCharacters(innerCharacters);
+                }
+            } 
+        }
     }, [characters])
 
     useEffect(()=>{
-        translateCharacters(orderedCharacters);
-    }, [orderedCharacters])
+        if(randomCharacters.length > 0) {
+            console.log('random: ', randomCharacters)
+            translateCharacters(randomCharacters).finally(() => {changeLoadingState(false)})
+        } else {
+            if(orderedCharacters.length > 0) {
+                console.log('ordered: ', orderedCharacters)
+                translateCharacters(orderedCharacters).finally(() => {changeLoadingState(false)})
+            }
+        }
+    }, [orderedCharacters, randomCharacters])
 
     return (
         <input
