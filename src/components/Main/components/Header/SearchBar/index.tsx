c75251fa9980e10f34ae6a5c1a644cae896b223b
JAMDev <jamdeveloper0@gmail.com>
2022-07-01 22:29:36 -0300

    Funcionamento dos filtros em conjunto com a pesquisa

--- src/components/Main/components/Header/SearchBar/index.tsx
+++ src/components/Main/components/Header/SearchBar/index.tsx
@@ -1,10 +1,16 @@
+import { TestTube } from "phosphor-react";
 import { useEffect, useState } from "react";
 
-import { useGetCharactersByPageLazyQuery, useGetCharactersByStringLazyQuery } from "../../../../../graphql/generated";
+
+import { useGetCharactersByPageLazyQuery, useGetCharactersByStatusAndNameLazyQuery, useGetCharactersByStatusLazyQuery, useGetCharactersByNameLazyQuery } from "../../../../../graphql/generated";
+
 import { useCurrentCharactersPage } from "../../../../../hooks/useCurrentCharactersPage";
+
 import { useIsLoading } from "../../../../../hooks/useIsLoading";
+import { useSearchFilter } from "../../../../../hooks/useSearchFilter";
 
 import { useTranslateCharacters } from "../../../../../hooks/useTranslateCharacters";
+
 
 interface characterInterface {
     name: string;
@@ -14,107 +20,286 @@ interface characterInterface {
     image: string;
 }
 
+
 export function SearchBar() {
     const [searchValue, setSearchValue] = useState<string>('');
 
-    const [randomCharacters, setRandomCharacters] = useState<characterInterface[]>([] as characterInterface[]);
 
-    const [characters, setCharacters] = useState<characterInterface[]>([] as characterInterface[]);
+    const [randomCharactersCaughtByPage, setRandomCharactersCaughtByPage] = useState<characterInterface[]>([] as characterInterface[]);
 
-    const [orderedCharacters,setOrderedCharacters] = useState<characterInterface[]>([] as characterInterface[]);
+    const [allRandomCharactersCaughtByStatus, setAllRandomCharactersCaughtByStatus] = useState<characterInterface[]>([] as characterInterface[]);
 
-    const [getCharactersByStringLazyQuery, { called, loading, data }] = useGetCharactersByStringLazyQuery();
+    const [pagedRandomCharactersCaughtByStatus, setPagedRandomCharactersCaughtByStatus] = useState<characterInterface[]>([] as characterInterface[]);
 
-    const [getCharactersByPageLazyQuery, props] = useGetCharactersByPageLazyQuery()
+    const [AllCharactersCaughtByName, setAllCharactersCaughtByName] = useState<characterInterface[]>([] as characterInterface[]);
+
+    const [allCharactersCaughtByNameAndStatus, setAllCharactersCaughtByNameAndStatus] = useState<characterInterface[]>([] as characterInterface[]);
+
+    const [pagedAndSortedCharactersCaughtByName, setPagedAndSortedCharactersCaughtByName] = useState<characterInterface[]>([] as characterInterface[]);
+    
+    const [pagedAndSortedCharactersCaughtByNameAndStatus, setPagedAndSortedCharactersCaughtByNameAndStatus] = useState<characterInterface[]>([] as characterInterface[]);
+
+
+    const [getCharactersByNameLazyQuery, nameProps] = useGetCharactersByNameLazyQuery();
+
+    const [getCharactersByPageLazyQuery, pageProps] = useGetCharactersByPageLazyQuery();
+
+    const [getCharactersByStatusLazyQuery, statusProps] = useGetCharactersByStatusLazyQuery();
+
+    const [getCharactersByStatusAndNameLazyQuery, statusAndNameProps] = useGetCharactersByStatusAndNameLazyQuery()
+
 
     const { translateCharacters} = useTranslateCharacters();
 
+
     const { changeLoadingState } = useIsLoading();
+
+
+    const { filter } = useSearchFilter();
+
 
     const { currentCharactersPage, setLastCharactersPage, setCurrentCharactersPage } = useCurrentCharactersPage()
 
-    useEffect(()=>{
-        if(searchValue !== '') {
-            changeLoadingState(true);
-            getCharactersByStringLazyQuery({variables: {
+
+    function getCharactersByStatusAndName() {
+        getCharactersByStatusAndNameLazyQuery({
+            variables: {
+                name: searchValue,
+                status: filter,
+                page: 1
+            }
+        })
+        .then(({ data }) => {
+            setAllCharactersCaughtByNameAndStatus(data?.characters?.results as characterInterface[])
+            setCurrentCharactersPage(1);
+        });
+    }
+
+    function getCharactersByStatus() {
+        getCharactersByStatusLazyQuery({
+            variables: {
+                page: 1,
+                status: filter
+            }
+        })
+        .then(({ data }) => {
+            setAllRandomCharactersCaughtByStatus(data?.characters?.results as characterInterface[])
+             setCurrentCharactersPage(1);
+        });
+    }
+
+
+    function getCharactersByName() {
+        getCharactersByNameLazyQuery({
+            variables: {
                 name: searchValue,
                 page: 1
-            }}).then( da => {setCharacters(da.data?.characters?.results as characterInterface[]); setRandomCharacters([] as characterInterface[])})
-        } else {
-            let randomNumber = Math.floor(Math.random() * 43)
+            }
+        })
+        .then(({ data }) => {
+            setAllCharactersCaughtByName(data?.characters?.results as characterInterface[])
+            setCurrentCharactersPage(1);
+        });
+    }
 
-            changeLoadingState(true);
-            getCharactersByPageLazyQuery({variables: {
+    function getRandomCharacters() {
+        let randomNumber = Math.floor(Math.random() * 43)
+    
+        getCharactersByPageLazyQuery({
+            variables: {
                 page: randomNumber
-            }}).then( da => {setRandomCharacters(da.data?.characters?.results as characterInterface[]); setCharacters([] as characterInterface[]); setCurrentCharactersPage(randomNumber)})
+            }
+        }).then(({ data }) => {
+            setRandomCharactersCaughtByPage(data?.characters?.results as characterInterface[])
+            setCurrentCharactersPage(randomNumber);
+        });
+    }
+
+
+    function sortCharacters(innerCharacters: characterInterface[], name: string) {
+        return innerCharacters.sort( (characterA, characterB) => {
+            if(characterA.name.toLocaleLowerCase().indexOf(name) > characterB.name.toLocaleLowerCase().indexOf(name)) {
+                return 1
+            }
+
+            if(characterA.name.toLocaleLowerCase().indexOf(name) < characterB.name.toLocaleLowerCase().indexOf(name)) {
+                return -1
+            }
+
+            if(characterA.name.toLocaleLowerCase() > characterB.name.toLocaleLowerCase()) {
+                return 1;
+            }
+
+            if(characterA.name.toLocaleLowerCase() < characterB.name.toLocaleLowerCase()) {
+                return -1;
+            }
+
+            return 0;
+        });
+    }
+
+
+    function charactersPagination(innerCharacters: characterInterface[], charactersPerPage: number, currentPage: number) {
+        return innerCharacters.slice(((charactersPerPage * currentPage) - charactersPerPage), (charactersPerPage * currentPage))
+    }
+
+
+
+    useEffect(()=>{
+        if(filter !== 'none') {
+            if(searchValue !== '') {
+                changeLoadingState(true);
+
+                getCharactersByStatusAndName();
+            } else {
+                changeLoadingState(true);
+
+                getCharactersByStatus();
+            }
+        } else {
+            if(searchValue !== '') {
+                changeLoadingState(true);
+
+                getCharactersByName();
+            } else {
+                changeLoadingState(true);
+                
+                getRandomCharacters();
+            }
         }
-    }, [searchValue])
+    }, [searchValue, filter])
+
 
     useEffect(() => {
-        if(randomCharacters.length > 0) {
+        if(searchValue === '' && filter === 'none') { 
             changeLoadingState(true);
-            getCharactersByPageLazyQuery({
-                variables: {
-                    page: currentCharactersPage
-                }
+            pageProps.refetch({
+                page: currentCharactersPage
             })
-            .then( da => setRandomCharacters(da.data?.characters?.results as characterInterface[]))
-            .finally(() => {changeLoadingState(false); setCurrentCharactersPage(currentCharactersPage)});
+            .then(({data}) => {
+                setRandomCharactersCaughtByPage(data.characters?.results as characterInterface[]);
+                changeLoadingState(false);
+            });
         }
     }, [currentCharactersPage])
 
+
     useEffect(()=> {
-        if(characters.length > 0) {
-            if(data?.characters?.info?.next) {
-                getCharactersByStringLazyQuery({variables: {
-                    name: searchValue,
-                    page: data?.characters?.info?.next
-                }}).then( data => {setCharacters( last => [...last, ...data.data?.characters?.results as characterInterface[]])})
-            } else {
-                if(characters.length > 0) {
-                    let innerCharacters = Array.from(characters);
+        if(searchValue === '' && filter !== 'none') {
+                if(statusProps.data?.characters?.info?.next) {
+                    getCharactersByStatusLazyQuery({
+                        variables: {
+                            page: statusProps.data?.characters?.info?.next,
+                            status: filter
+                        }
+                    })
+                    .then( data => {
+                        setAllRandomCharactersCaughtByStatus( last => [...last, ...data.data?.characters?.results as characterInterface[]])
+                    });
+                } else {
+                    let innerCharacters = Array.from(allRandomCharactersCaughtByStatus);
+    
+                    setLastCharactersPage(Math.ceil(innerCharacters.length/20))
+
+                    innerCharacters = charactersPagination(innerCharacters, 20, currentCharactersPage);
+    
+
+                    setPagedRandomCharactersCaughtByStatus(innerCharacters);
+                }
+        }
+    }, [allRandomCharactersCaughtByStatus, currentCharactersPage])
+
+
+
+
+    useEffect(()=> {
+        if(searchValue !== '' && filter !== 'none') {
+                if(statusAndNameProps.data?.characters?.info?.next) {
+                    getCharactersByStatusAndNameLazyQuery({variables: {
+                        name: searchValue,
+                        page: statusAndNameProps.data?.characters?.info?.next,
+                        status: filter
+                    }}).then( data => {setAllCharactersCaughtByNameAndStatus( last => [...last, ...data.data?.characters?.results as characterInterface[]]); console.log('Meio')})
+                } else {
+                    let innerCharacters = Array.from(allCharactersCaughtByNameAndStatus);
     
                     let lowercasedSearchValue = searchValue.toLocaleLowerCase();
 
+    
                     setLastCharactersPage(Math.ceil(innerCharacters.length/20))
                     
-                    innerCharacters = innerCharacters.sort( (characterA, characterB) => {
-                        if(characterA.name.toLocaleLowerCase().indexOf(lowercasedSearchValue) > characterB.name.toLocaleLowerCase().indexOf(lowercasedSearchValue)) {
-                            return 1
-                        }
-            
-                        if(characterA.name.toLocaleLowerCase().indexOf(lowercasedSearchValue) < characterB.name.toLocaleLowerCase().indexOf(lowercasedSearchValue)) {
-                            return -1
-                        }
+                    innerCharacters = sortCharacters(innerCharacters, lowercasedSearchValue);
+
+                    innerCharacters = charactersPagination(innerCharacters, 20, currentCharactersPage);
     
-                        if(characterA.name.toLocaleLowerCase() > characterB.name.toLocaleLowerCase()) {
-                            return 1;
-                        }
-    
-                        if(characterA.name.toLocaleLowerCase() < characterB.name.toLocaleLowerCase()) {
-                            return -1;
-                        }
-            
-                        return 0
-                    }).slice((20 * currentCharactersPage) - 20, 20 * currentCharactersPage);
-    
-                    setOrderedCharacters(innerCharacters);
+
+                    setPagedAndSortedCharactersCaughtByNameAndStatus(innerCharacters);
                 }
+        }
+    }, [allCharactersCaughtByNameAndStatus, currentCharactersPage])
+
+
+
+    useEffect(()=> {
+        if(searchValue !== '' && filter === 'none') {
+            if(nameProps.data?.characters?.info?.next) {
+                getCharactersByNameLazyQuery({variables: {
+                    name: searchValue,
+                    page: nameProps.data?.characters?.info?.next
+                }}).then( data => {setAllCharactersCaughtByName( last => [...last, ...data.data?.characters?.results as characterInterface[]])})
+            } else {
+                let innerCharacters = Array.from(AllCharactersCaughtByName);
+
+                let lowercasedSearchValue = searchValue.toLocaleLowerCase();
+
+
+                setLastCharactersPage(Math.ceil(innerCharacters.length/20))
+                
+                innerCharacters = sortCharacters(innerCharacters, lowercasedSearchValue);
+
+                innerCharacters = charactersPagination(innerCharacters, 20, currentCharactersPage);
+
+
+                setPagedAndSortedCharactersCaughtByName(innerCharacters);
             } 
         }
-    }, [characters, currentCharactersPage])
+    }, [AllCharactersCaughtByName, currentCharactersPage])
+
 
     useEffect(()=>{
-        if(randomCharacters.length > 0) {
-            console.log('random: ', randomCharacters)
-            translateCharacters(randomCharacters).finally(() => {changeLoadingState(false)})
-        } else {
-            if(orderedCharacters.length > 0) {
-                console.log('ordered: ', orderedCharacters)
-                translateCharacters(orderedCharacters).finally(() => {changeLoadingState(false)})
-            }
+        if(searchValue !== '' && filter !== 'none') {
+            translateCharacters(pagedAndSortedCharactersCaughtByNameAndStatus).finally(() => {
+                changeLoadingState(false) //Talvez colocar um to no final do nome desse metódo
+            })
         }
-    }, [orderedCharacters, randomCharacters])
+    }, [pagedAndSortedCharactersCaughtByNameAndStatus])
+
+    useEffect(()=>{
+        if(searchValue !== '' && filter === 'none') {
+            translateCharacters(pagedAndSortedCharactersCaughtByName).finally(() => {
+                changeLoadingState(false) //Talvez colocar um to no final do nome desse metódo
+            })
+        }
+    }, [pagedAndSortedCharactersCaughtByName])
+
+
+    useEffect(() => {
+        if(searchValue === '' && filter === 'none') {
+            translateCharacters(randomCharactersCaughtByPage).finally(() => {
+                changeLoadingState(false)
+            })
+        }
+    }, [randomCharactersCaughtByPage])
+
+
+    useEffect(() => {
+        if(searchValue === '' && filter !== 'none') {
+            translateCharacters(pagedRandomCharactersCaughtByStatus).finally(() => {
+                changeLoadingState(false)
+            })
+        }
+    }, [pagedRandomCharactersCaughtByStatus])
+
 
     return (
         <input
