cd7c0d808cc78624e661e2c3552abb71b6fef953
JAMDev <jamdeveloper0@gmail.com>
2022-07-03 19:10:56 -0300

    Definição de quando e com que conteudo o NoCharactersFound deve renderizar

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -6,26 +6,59 @@ import { CharacterCards } from "./components/CharacterCards";
 
 import { Header } from "./components/Header";
 
+import { NoCharactersFound } from "./components/NoCharactersFound";
+
 
 import { useIsLoading } from "../../hooks/useIsLoading";
+
+import { useTranslateCharacters } from "../../hooks/useTranslateCharacters";
+import { useSearchFilter } from "../../hooks/useSearchFilter";
 
 
 export function Main() {
     const { isLoading } = useIsLoading()
+
+    const { translatedCharacters } = useTranslateCharacters();
     
+    const { filter } = useSearchFilter()
+
+
+    function translateFilter() {
+        switch(filter) {
+            case 'alive':
+                return 'vivo'
+
+            case 'dead':
+                return 'morto'
+
+            case 'unknown':
+                return 'desconhecido'
+        }
+    }
+
+
     return (
         <main 
          className='
-            flex flex-col p-8 w-full
+            flex flex-col p-8 w-full relative
          '
         >
             <Header/>
             {
                 !isLoading ?
-                    <>
-                        <CharacterCards />
-                        <Navigation/>
-                    </>
+                    translatedCharacters.length > 0 ? 
+                        <>
+                            <CharacterCards />
+                            <Navigation/>
+                        </>
+                    :
+                        filter === 'none' ?
+                            <NoCharactersFound message="Ops, parece que não foi possivel encontrar alguém com esse nome."/>
+                        :
+                            <NoCharactersFound
+                             message={`Ops, parece que não foi possivel encontrar alguém com status "${translateFilter()}" com esse nome.`}
+                            />
+
                 :
                     <div
                      className="
