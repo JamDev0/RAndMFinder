37570d1298d2077d217ca35e8058df51457d4fe9
JAMDev <jamdeveloper0@gmail.com>
2022-07-07 18:08:23 -0300

    Melhoria no visual do feedback de loading

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -6,7 +6,7 @@ export function Loading() {
     return (
         <motion.div 
          className="
-            w-full h-auto
+            w-full h-auto max-w-[200px]
          "
          animate={{ rotate: 360 }}
          transition={{ duration: 0.9, ease: 'backInOut', repeat: Infinity }}
